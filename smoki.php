0f9fcec5b7fb0ae0ea639c6678396d6406e158ee
Unecomic <mateusztryka474@gmail.com>
2025-10-28 13:50:40 +0100

    Add files via upload

---
+++ smoki.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Smoki</title>
+
+    <link rel="stylesheet" href="styl.css">
+</head>
+<body>
+    
+    <nav>
+
+
+        <h2>
+Pozanaj smoki
+        </h2>
+
+
+        <div id=div1>
+<textarea>
+Baza
+</textarea>
+        </div>
+
+        <div id=div2>
+<textarea>
+Opisy
+</textarea>
+        </div>
+
+        
+        <div id=div3>
+<textarea>
+Galeria
+</textarea>
+        </div>
+
+    </nav>
+
+    <main>
+
+        <section id="section1">
+<h3>
+
+    Baza Smoków
+</h3>
+<form action="smoki.php" method="post">
+
+
+    <?php
+
+
+
+    ?>
+    <input type="submit" name="Szukaj" id="Szukaj">
+</form>
+<table>
+
+</table>
+        </section>
+
+   <section id="section2">
+<h3>
+
+    Opisy smoków
+</h3>
+
+<dl>
+
+
+    <dt>
+Smok czerwony
+
+    </dt>
+
+    <dd>
+Pochodzi z Chin. Ma 1000 lat. Żywi się mniejszymi zwierzętami. Posiada łuski cenne na rynkach wschodnich do wyrabiania lekarstw. Jest dziki i groźny.
+    </dd>
+
+  <dt>
+
+Smok zielony
+    </dt>
+
+    <dd>
+Pochodzi z Bułgarii. Ma 10000 lat. Żywi się mniejszymi zwierzętami, ale tylko w kolorze zielonym. Jest kosmaty. Z sierści zgubionej przez niego, tka się najdroższe materiały.
+    </dd>
+
+      <dt>
+Smok niebieski 
+
+    </dt>
+
+    <dd>
+Pochodzi z Francji. Ma 100 lat. Żywi się owocami morza. Jest natchnieniem dla najlepszych malarzy. Często im pozuje. Smok ten jest przyjacielem ludzi i czasami im pomaga. Jest jednak próżny i nie lubi się przepracowywać.
+    </dd>
+
+
+</dl>
+
+        </section>
+           <section id="section3">
+
+<h3>
+    Galeria
+</h3>
+
+<img src="smok1.jpg" alt="smok czerwony">
+<img src="smok2.jpg" alt="smok wielki">
+<img src="smok3.jpg" alt="skrzydlaty łaciaty">
+
+
+        </section>
+
+    </main>
+
+    <footer>
+<p>
+
+    Stronę opracował:12
+</p>
+    </footer>
+</body>
+</html>
